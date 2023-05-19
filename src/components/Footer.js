cc40e1fe8efadf4c0821a4b4976f97ef9a47b87d
Sayed Kumail <sayyedkumailabbas363@gmail.com>
2023-05-19 12:50:07 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+  render() {
+    return (
+      
+        <div className="text-bg-light p-3 text-center"> Copyright &copy; 2023 Kumail Rizvi. All rights reserved.</div>
+      
+    )
+  }
+}
+
+export default Footer
